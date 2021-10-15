6c660f7c2e043d17d95894bbc2c93068c846a03f
emwitthun <witthun1759@uwlax.edu>
2021-10-15 07:35:49 -0500

    More changes to account create

--- NonprofitWebsite/src/app/account-create/account-create.component.ts
+++ NonprofitWebsite/src/app/account-create/account-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ValidatorFn, AbstractControl,
+  ValidationErrors} from '@angular/forms';
+// import { randomInt } from 'crypto';
 
 @Component({
   selector: 'app-account-create',
@@ -7,9 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountCreateComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  passwordsDontMatch: boolean = false;
+  isUsernameTaken: boolean = false;
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      Username: ['', Validators.required, uniqueUsernameValidator()],
+      Password: ['', Validators.required],
+      PasswordCheck: ['', Validators.required]
+    });
+  }
+
+  
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    this.createAccount(this.form.get('Username')?.value, this.form.get('Password')?.value, this.form.get('PasswordCheck')?.value);
+  }
+
+  createAccount(Username: String, Password: String, PasswordCheck: String){
+    // Password double check
+    if(Password === PasswordCheck){
+      this.passwordsDontMatch = false;
+      // hash the password
+      var bcrypt = require('bcryptjs');
+      // create salt
+      // var salt = bcrypt.genSaltSync(randomInt(Number.MAX_SAFE_INTEGER));
+      // var hash = bcrypt.hashSync(Password, salt);
+      //TODO store salt and hash in database
+    }
+    else
+    {
+      this.passwordsDontMatch = true;
+      return;
+    }
+  }
+}
+
+/* Username must be unique */
+function uniqueUsernameValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    //TODO query database for username
+    const isInDatabaseAlready = true;
+    return isInDatabaseAlready ? { usernameTaken: { value: true } } : null;
+  };
 }
