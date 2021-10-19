809d94386520dc7dd487bd2ff7e53bd8763ebca0
emwitthun <witthun1759@uwlax.edu>
2021-10-19 15:56:20 -0500

    intial home component dynamic form

--- NonprofitWebsite/src/app/home/home.component.ts
+++ NonprofitWebsite/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray,
+  FormControl, ValidatorFn
+} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  ordersData = [
+    { id: 100, name: 'order 1' },
+    { id: 200, name: 'order 2' },
+    { id: 300, name: 'order 3' },
+    { id: 400, name: 'order 4' }
+  ];
+
+  get ordersFormArray() {
+    return this.form.controls.orders as FormArray;
+  }
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      orders: new FormArray([])
+    });
+
+    this.addCheckboxes();
+  }
+
+  private addCheckboxes() {
+    this.ordersData.forEach(() => this.ordersFormArray.push(new FormControl(false)));
+  }
+
+  submit() {
+    console.log("test");
+  }
 
   ngOnInit(): void {
   }
