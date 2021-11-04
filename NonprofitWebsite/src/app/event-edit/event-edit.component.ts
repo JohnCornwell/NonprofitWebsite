443e3dd709f7f6a80190c06c32116da049a9b869
emwitthun <witthun1759@uwlax.edu>
2021-11-04 00:36:58 -0500

    Added temorary back link from event edit

--- NonprofitWebsite/src/app/event-edit/event-edit.component.ts
+++ NonprofitWebsite/src/app/event-edit/event-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-event-edit',
@@ -10,10 +11,14 @@ export class EventEditComponent implements OnInit {
 
   ID: number = 0;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.ID = this.route.snapshot.params.id;
+  }
+
+  navigateToAdminHome() {
+    this.router.navigate(['/Home/admin']);
   }
 
 }
