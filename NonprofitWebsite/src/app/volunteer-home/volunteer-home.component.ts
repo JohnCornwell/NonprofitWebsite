ad2d6ed0a8a0e5a9de43463fc282a7f322ea9d10
emwitthun <witthun1759@uwlax.edu>
2021-11-06 20:12:14 -0500

    Attempts at volunteer home

--- NonprofitWebsite/src/app/volunteer-home/volunteer-home.component.ts
+++ NonprofitWebsite/src/app/volunteer-home/volunteer-home.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Event } from '../interfaces/Event';
 
 @Component({
   selector: 'app-volunteer-home',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./volunteer-home.component.css']
 })
 export class VolunteerHomeComponent implements OnInit {
+  eventsList: Array<Event> = new Array<Event>();
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+
+  }
+
+  volunteer(ID: Number){
+    console.log("Volunteer "+ ID);
+  }
 
   ngOnInit(): void {
+    console.log("ID: " + sessionStorage.getItem("id"));
+    this.http.get<any>("/event/list", { observe: "response" }).subscribe(result => {
+      if (result.status != 200) {
+        window.alert("Error in requesting event list from server.");
+      } else {
+        this.eventsList = result.body;
+      }
+    });
+    var body = {
+      // User: sessionStorage.getItem("name"),
+      UserId: Number(sessionStorage.getItem("id"))
+      // UserType: sessionStorage.getItem("type")
+    }
+    console.log(body);
+    this.http.post<any>("/volunteers/retrieveEvents", body, { observe: "response" }).subscribe(result => {
+      if (result.status != 200) {
+        window.alert("Error in requesting volunteers list from server.");
+      } else {
+        console.log(result.body);
+      }
+    });
   }
 
 }
